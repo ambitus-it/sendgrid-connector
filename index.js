8ee35b773ed98f40a2ef829fac59f929388b8f04
Caio Rodrigues <caio@lap3d.com.br>
2019-06-25 14:20:00 -0300

    lets encrypt in server

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const app = require('./config/custom-express')();
-let port = process.env.PORT || 3000;
+const fs = require('fs');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/api2.singularegestao.com.br/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/api2.singularegestao.com.br/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/api2.singularegestao.com.br/chain.pem', 'utf8');
 
-app.listen(port, function () {
-     console.log("Sendgrid connector running on Port: " + port);
- });
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
