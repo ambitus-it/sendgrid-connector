64aa202bc8717bbca93a566b92670a94dec0b40e
Bruno Benicio <bruno@lap3d.com.br>
2019-08-24 20:50:43 -0300

    add mail

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const servico = require('../controller/index')
 
-router.get('/', function (req, res) {
+router.get('/mail', function (req, res) {
   res.send({ working: true })
 });
 
-router.post('/email', function (req, res) {
+router.post('/mail/send', function (req, res) {
   try {
     const msg = {
         to,
@@ -17,6 +17,7 @@ router.post('/email', function (req, res) {
     } = req.body;
   
     const retEmail = servico.sendEmail(msg.to, msg.from, msg.subject, msg.text, msg.templateId, msg.params);
+
     res.status(200).send(retEmail)
   } catch(e) {
     console.log(e)
